e1eb797524493c8e77a6fe950add2d7364e223be
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-28 04:21:03 -0300

    update meu-aprendizado01/treino-004/assets/js/Index.js

---
+++ meu-aprendizado01/treino-004/assets/js/Index.js
@@ -0,0 +1,13 @@
+
+
+
+
+//var global
+var btn = window.document.getElementsByClassName("button")[0]
+
+//Events listeners
+btn.addEventListener("click", clicou);
+
+function clicou(){
+    button.innerText = "Concluido"
+}
