d1795e2ef46293f03f4a0b85d9f9facf43f4d3d8
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-28 20:10:28 -0300

    update meu-aprendizado01/treino-005/assets/javascript/Index.js

--- meu-aprendizado01/treino-005/assets/javascript/Index.js
+++ meu-aprendizado01/treino-005/assets/javascript/Index.js
@@ -6,9 +6,19 @@ botão1.addEventListener("touchstart", finalizado);
 function finalizado(){
     botão1.value = "Finalizado"
     botão1.style.transition = "500ms"
-    setTimeout(concluir, 2000);
+    setTimeout(concluir, 500);
+    function concluir(){
+        botão1.value = "Concluir"
+    }
 }
 
-function concluir(){
-    botão1.value = "concluir"
-}
+
+botão2.addEventListener("touchstart", deletado);
+function deletado(){
+    botão2.value = "Deletado"
+    botão2.style.transition = "500ms"
+    setTimeout(deletar, 500);
+    function deletar(){
+        botão2.value = "Deletar"
+    }
+}    
