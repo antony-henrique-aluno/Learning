c2fd9b9a7ffea392a9722152cd8132765b047d15
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-29 18:34:54 -0300

    update meu-aprendizado01/treino-002/assets/js/app.js

---
+++ meu-aprendizado01/treino-002/assets/js/app.js
@@ -0,0 +1,5 @@
+//Primeiro crie a variavel que vai reber o id do elemento 
+const btn = querySelector("#btn1");
+
+//Segundo adicione os events listeners na variavel que recebeu o id do elemento 
+btn.addEventListener("t", )
