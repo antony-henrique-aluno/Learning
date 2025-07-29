9f3c940ddf87afb4f3df69ea9cae810fc4baabf8
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-29 18:45:22 -0300

    update meu-aprendizado01/treino-002/assets/js/app.js

--- meu-aprendizado01/treino-002/assets/js/app.js
+++ meu-aprendizado01/treino-002/assets/js/app.js
@@ -1,8 +1,11 @@
 //Primeiro crie a variavel que vai reber o id do elemento 
-const btn = querySelector("#btn1");
+const btn = document.querySelector("#btn1");
 
 //Segundo adicione os events listeners na variavel que recebeu o id do elemento 
 btn.addEventListener("touchstart", toque);
 
 //Terceiro crie a função que vai disparar o evento
-function
+function toque(){
+    btn.innerText = "Kkkkkkk"
+    btn.style.background = "Blue";
+}
