2ab9edb0ddcee7fc7b10c27ab14a9ef14795409a
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-30 16:52:50 -0300

    update meu-aprendizado01/treino-006/assets/js/app.js

---
+++ meu-aprendizado01/treino-006/assets/js/app.js
@@ -0,0 +1,5 @@
+//Pegando e criandos a variaveis dos elementos
+const miniatura = document.querySelector(".miniatura");
+const modal = document.querySelector("#modal");
+const imagemAmpliada = document.querySelector("#imagem-ampliada");
+const fechar = document.querySelector("#fechar");
