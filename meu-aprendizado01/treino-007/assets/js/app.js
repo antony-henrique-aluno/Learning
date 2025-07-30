6d8c538ad5ec726025f1850936b3ace669f79ea5
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-30 20:34:34 -0300

    update meu-aprendizado01/treino-007/assets/js/app.js

--- meu-aprendizado01/treino-007/assets/js/app.js
+++ meu-aprendizado01/treino-007/assets/js/app.js
@@ -2,7 +2,7 @@ const img = document.querySelectorAll(".zoom-img");
 const modal = document.querySelector("#zoom-modal");
 const zoomedImg = document.querySelector("#zoomed-img");
 
-img.forEach(function(imagens){
+img.forEach(function(imagens){ //Preciso estudar sobre forEach
     imagens.addEventListener("click", function(){
         zoomedImg.src = imagens.src;
         modal.style.display = "block";
