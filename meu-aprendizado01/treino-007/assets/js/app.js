f1bdcc2ade829db30f78da03e29e1696215caf46
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-30 19:47:59 -0300

    update meu-aprendizado01/treino-007/assets/js/app.js

--- meu-aprendizado01/treino-007/assets/js/app.js
+++ meu-aprendizado01/treino-007/assets/js/app.js
@@ -1,3 +1,10 @@
 const img = document.querySelectorAll(".zoom-img");
 const modal = document.querySelector("#zoom-modal");
 const zoomedImg = document.querySelector("#zoomed-img");
+
+img.forEach(function(imagens){
+    imagens.addEventListener("click", function(){
+        zoomedImg.src = img.src;
+        modal.style.display ="block";
+    })
+})
