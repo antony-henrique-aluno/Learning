13cae8bf095656984a84d3b777497dff9893d76a
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-30 20:39:05 -0300

    update meu-aprendizado01/treino-007/assets/js/app.js

--- meu-aprendizado01/treino-007/assets/js/app.js
+++ meu-aprendizado01/treino-007/assets/js/app.js
@@ -1,6 +1,7 @@
 const img = document.querySelectorAll(".zoom-img");
 const modal = document.querySelector("#zoom-modal");
 const zoomedImg = document.querySelector("#zoomed-img");
+const fechado = document.querySelector("#fechar");
 
 img.forEach(function(imagens){ //Preciso estudar sobre forEach
     imagens.addEventListener("click", function(){
@@ -8,3 +9,9 @@ img.forEach(function(imagens){ //Preciso estudar sobre forEach
         modal.style.display = "block";
     });
 });
+
+//Botão de fechar
+fechado.addEventListener("click", close);
+function close(){
+    modal.style.display = "none";
+}
