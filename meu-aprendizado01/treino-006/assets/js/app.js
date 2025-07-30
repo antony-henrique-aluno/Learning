6015ee6a7aaafc6873c7a8120763964656c7f19c
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-30 16:57:43 -0300

    update meu-aprendizado01/treino-006/assets/js/app.js

--- meu-aprendizado01/treino-006/assets/js/app.js
+++ meu-aprendizado01/treino-006/assets/js/app.js
@@ -3,3 +3,9 @@ const miniatura = document.querySelector(".miniatura");
 const modal = document.querySelector("#modal");
 const imagemAmpliada = document.querySelector("#imagem-ampliada");
 const fechar = document.querySelector("#fechar");
+
+//Criando o evento de clique e touch
+miniatura.addEventListener("click", clique);
+miniatura.addEventListener("touchstart", clique);
+
+function 
