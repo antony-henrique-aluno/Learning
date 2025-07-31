e12668fe814b6d7f49a62a5f7b9704cda29c48e4
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-31 00:09:39 -0300

    update meu-aprendizado01/treino-008/assets/js/app.js

--- meu-aprendizado01/treino-008/assets/js/app.js
+++ meu-aprendizado01/treino-008/assets/js/app.js
@@ -2,3 +2,10 @@ const miniaturaImg = document.querySelector("#miniatura");
 const modalImg = document.querySelector("modal");
 const zoomImg = document.querySelector("#zoom-img");
 
+//Criando o evento de clique para o zoom na imagem 
+miniaturaImg.addEventListener("click", zoom);
+miniaturaImg.addEventListener("touchstart", zoom);
+function zoom(){
+    modalImg.style.display = "block";
+    zoomImg.src = miniaturaImg.src;
+}
