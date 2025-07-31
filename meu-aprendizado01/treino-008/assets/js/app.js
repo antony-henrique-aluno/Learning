0cfb75288242d86302af43932bb2fc135ad4d018
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-30 23:39:25 -0300

    update meu-aprendizado01/treino-008/assets/js/app.js

---
+++ meu-aprendizado01/treino-008/assets/js/app.js
@@ -0,0 +1,5 @@
+const miniatura = document.querySelector("#miniatura");
+const modal = document.querySelector("#modal");
+const zoomImg = document.querySelector("#zoom-img");
+
+//Criando o evento de clique
