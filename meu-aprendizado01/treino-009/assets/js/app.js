28a7462290cf7922a639cfbdac2f3365b1453375
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-31 20:14:42 -0300

    update meu-aprendizado01/treino-009/assets/js/app.js

--- meu-aprendizado01/treino-009/assets/js/app.js
+++ meu-aprendizado01/treino-009/assets/js/app.js
@@ -3,7 +3,6 @@ function qtdNome(){
     const nome = document.querySelector("#nome")
     const modalNome = document.querySelector("#nome-modal") 
     if (nome.value.length <= 8) {
-        modalNome.innerHTML = "Senha muito fraca";
-        
+        modalNome.innerHTML = "Nome muito pequeno";
     }
 }
