6ec3760df5c1bb601942b01a5970031b3da771d9
antony-henrique-aluno <meugithub220@gmail.com>
2025-07-31 20:39:42 -0300

    update meu-aprendizado01/treino-009/assets/js/app.js

--- meu-aprendizado01/treino-009/assets/js/app.js
+++ meu-aprendizado01/treino-009/assets/js/app.js
@@ -1,10 +1,10 @@
 const nome = document.querySelector("#nome");
 const modalNome = document.querySelector("#modal-nome");
-const input = document.querySelector("#input-submit"); 
+const input = document.querySelector(".formulario"); 
 
 input.addEventListener("submit", qtdletras);
 
-function qtdletras(){
+function qtdletras(event){
     if (nome.value.length <= 8) {
         modalNome.innerHTML = "Sou fracassado"
     }
