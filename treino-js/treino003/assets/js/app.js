5318a9354d501a6a93a57e0d7e2060db6d7dcbb7
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-01 20:11:47 -0300

    update treino-js/treino003/assets/js/app.js

--- treino-js/treino003/assets/js/app.js
+++ treino-js/treino003/assets/js/app.js
@@ -1,3 +1,12 @@
 const caixaPergunta = document.querySelector("#pergunta");
 const btnConfirme = document.querySelector("#btn");
-const boxResult = document.querySelector(".resultado")
+const boxResult = document.querySelector(".resultado");
+
+btnConfirme.addEventListener("click", clique);
+
+function clique(){
+    if (caixaPergunta == brasil) {
+        boxResult.innerHTML = "Deu certo?"
+    }else 
+        boxResult.innerHTML = "Foda"
+}
