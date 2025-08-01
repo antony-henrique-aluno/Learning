af6b9e944bfcdd6e8fbe76e19915794f1ca321b2
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-01 15:56:03 -0300

    update treino-js/treino002/assets/js/app.js

--- treino-js/treino002/assets/js/app.js
+++ treino-js/treino002/assets/js/app.js
@@ -1,11 +1,16 @@
 const miniatura = document.querySelector("#ftMiniatura");
 const modal = document.querySelector("#modal");
 const zoomImg = document.querySelector("#zoomImg");
-
+const fechar = document.querySelector("#fechar");
 
 //Criando a função para dar o zoom na imagem 
 miniatura.addEventListener("click", abrirImg) //A variavel miniatura recebe o evento click 
+fechar.addEventListener("click", fechandoImg);
 function abrirImg(){
     modal.style.display = "block" 
+    modal.style.background = "rgba(0, 0, 0, 0.5)"
     zoomImg.src = miniatura.src
+    function fechandoImg(){
+        modal.style.display = "none";
+    }
 }
