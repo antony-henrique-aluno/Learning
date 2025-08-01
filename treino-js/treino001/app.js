e293c285c141a0ca6a4b316ff58e1dd40297c140
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-01 14:55:46 -0300

    update treino-js/treino001/app.js

--- treino-js/treino001/app.js
+++ treino-js/treino001/app.js
@@ -2,13 +2,24 @@
 
 const temperatura = 36
 if (temperatura < 0) {
-    console.log("Está congelando aqui!")
+     console.log("Está congelando aqui!")
 }else if (temperatura >= 0 && temperatura <= 25) {
-    console.log("O clima está agradavel") //Mostra na tela oq foi digitado
+     console.log("O clima está agradavel") //Mostra na tela oq foi digitado
 }else if (temperatura > 25 && temperatura <= 35) {
-    console.log("O clima está ficando quente")
+     console.log("O clima está ficando quente")
 }else 
-    console.log("Está pegando fogo, bixo!")
+     console.log("Está pegando fogo, bixo!")
     
     
 //Classificando temperatura do maior valor, até o menor
+
+const temp = -5
+if (temp > 35) {
+    console.log("Está pegando fogo! bixo")
+}else if (temp >= 25) {
+    console.log("O clima está agradavel")
+}else if (temp >= 0){
+    console.log("Que clima agradavel")
+}else
+    console.log("Esta tudo congelando")
+    
