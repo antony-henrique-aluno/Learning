5e519019be1932569f30e6fb759a880afa686210
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-01 15:09:26 -0300

    update meu-aprendizado01/treino-009/assets/js/app.js

--- meu-aprendizado01/treino-009/assets/js/app.js
+++ meu-aprendizado01/treino-009/assets/js/app.js
@@ -1,7 +1,8 @@
 const nome = document.querySelector("#nome");
+const senha = document.querySelector("#senha");
 const modalNome = document.querySelector("#modal-nome");
 const input = document.querySelector(".formulario"); 
-const ModalSenha = document.querySelector("#modal-senhs");
+const ModalSenha = document.querySelector("#modal-senha");
 
 input.addEventListener("submit", qtdletras);
 
@@ -12,7 +13,8 @@ function qtdletras(event){
 }
 
 function qtdletras(event){
-    if (nome.value.length <= 8){
-        ModalSenha.innerHTML = "Senha muito pequena"
+    if (senha.value.length <= 8){
+        ModalSenha.innerHTML = "Senha muito pequena";
+        senha.style.borderBottom = "2px solid red";
     }
 }
