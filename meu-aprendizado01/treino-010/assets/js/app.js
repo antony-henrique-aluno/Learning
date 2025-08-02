3b5b1f78654fd32fe3ee51eab7801255037ef1d2
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-02 01:59:15 -0300

    update meu-aprendizado01/treino-010/assets/js/app.js

---
+++ meu-aprendizado01/treino-010/assets/js/app.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("#submit");
+const formNome = document.querySelector("#nome");
+const formSenha = document.querySelector("#senha");
+const resultadoNome = document.querySelector("#boxAviso1");
+form
