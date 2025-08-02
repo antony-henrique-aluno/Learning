590fa16811bbe5fce72bd77b42c8cc1288bf7786
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-02 02:01:40 -0300

    update meu-aprendizado01/treino-010/assets/js/app.js

--- meu-aprendizado01/treino-010/assets/js/app.js
+++ meu-aprendizado01/treino-010/assets/js/app.js
@@ -2,4 +2,14 @@ const form = document.querySelector("#submit");
 const formNome = document.querySelector("#nome");
 const formSenha = document.querySelector("#senha");
 const resultadoNome = document.querySelector("#boxAviso1");
-form
+const resultadoSenha = document.querySelector("#boxAviso2");
+
+//Criando o Evento
+form.addEventListener("submit", clique);
+
+//Criando a função submit
+function clique(){
+    if (formNome) {
+        
+    }
+}
