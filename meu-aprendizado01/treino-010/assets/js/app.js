43e8dcf5c61f7047b0be942b6a571460cb54cc36
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-02 05:30:14 -0300

    update meu-aprendizado01/treino-010/assets/js/app.js

--- meu-aprendizado01/treino-010/assets/js/app.js
+++ meu-aprendizado01/treino-010/assets/js/app.js
@@ -5,31 +5,54 @@ const resultadoNome = document.querySelector("#boxAviso1");
 const resultadoSenha = document.querySelector("#boxAviso2");
 
 //Criando o Evento
-form.addEventListener("submit", clique);
-
-//Criando a função submit
-function clique(){
-    if (formNome.value.length < 8) {
-        resultadoNome.innerHTML = "Nome muito pequeno! melhore.";
-        formNome.style.borderBottom = "2px solid red";
+form.addEventListener("submit", function(event){
+    event.preventDefault()//Parametro que impede o recarregamento da pagina
+    //Logica da validação do nome
+    if (formNome.value.length <= 8) {
+        resultadoNome.innerHTML = "Seu nome tem poucas letras"
+        formNome.style.borderBottom = "2px solid rgb(255,14,14)";
     }else if (formNome.value.length > 8 && formNome.value.length <= 16) {
-        resultadoNome.innerHTML = "Quantidade de letras aceitas";
-        formNome.style.borderBottom = "2px solid orange";
-    }else{
-        resultadoNome.innerHTML = "O nome está forte!";
-        formNome.style.borderBottom = "2px solid rgb(14,255,19)";
+        resultadoNome.innerHTML = "Seu nome tem uma Quantidade de letras rasoaveis"
+        formNome.style.borderBottom = "2px solid rgb(255,160,14)";
     }
 }
 
-function clique(){
-    if (formSenha.value.length < 8) {
-        resultadoSenha.innerHTML = "Senha muito fraca, melhore!";
-        formSenha.style.borderBottom = "2px solid red";
-    }else if (formSenha.value.length > 8 && formSenha.value.length <= 16) {
-        resultadoSenha.innerHTML = "Senha de tamanho rasoavel";
-        formSenha.style.borderBottom = "2px solid orange";
-    }else{
-        resultadoSenha.innerHTML = "A senha está forte!";
-        formSenha.style.borderBottom = "2px solid rgb(14,255,19)";
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function clique(){
+//     if (formNome.value.length < 8) {
+//         resultadoNome.innerHTML = "Nome muito pequeno! melhore.";
+//         formNome.style.borderBottom = "2px solid red";
+//     }else if (formNome.value.length > 8 && formNome.value.length <= 16) {
+//         resultadoNome.innerHTML = "Quantidade de letras aceitas";
+//         formNome.style.borderBottom = "2px solid orange";
+//     }else{
+//         resultadoNome.innerHTML = "O nome está forte!";
+//         formNome.style.borderBottom = "2px solid rgb(14,255,19)";
+//     }
+// }
+// 
+// function clique(){
+//     if (formSenha.value.length < 8) {
+//         resultadoSenha.innerHTML = "Senha muito fraca, melhore!";
+//         formSenha.style.borderBottom = "2px solid red";
+//     }else if (formSenha.value.length > 8 && formSenha.value.length <= 16) {
+//         resultadoSenha.innerHTML = "Senha de tamanho rasoavel";
+//         formSenha.style.borderBottom = "2px solid orange";
+//     }else{
+//         resultadoSenha.innerHTML = "A senha está forte!";
+//         formSenha.style.borderBottom = "2px solid rgb(14,255,19)";
+//     }
+// }
