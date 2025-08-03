1c22fb1101d88bf8fd7ca39c1bb8f8765ced53d8
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-03 14:32:16 -0300

    update treino-js/treino004/assets/js/app.js

--- treino-js/treino004/assets/js/app.js
+++ treino-js/treino004/assets/js/app.js
@@ -10,7 +10,6 @@ form.addEventListener("submit", function(event){
     event.preventDefault();
     if (inputNome.value.length < 8) {
         aviso1.innerHTML = "Nome muito curto";
-        inputNome.style.borderBottom = "2px solid red";
     }
     if (inputSenha.value.length < 8) {
         aviso2.innerHTML = "Senha muito curta";
