d64a5ce4d373fa3176224d30503b28082b13375f
antony-henrique-aluno <meugithub220@gmail.com>
2025-08-03 14:25:12 -0300

    update treino-js/treino004/assets/js/app.js

--- treino-js/treino004/assets/js/app.js
+++ treino-js/treino004/assets/js/app.js
@@ -8,8 +8,11 @@ const aviso2 = document.querySelector("#BoxAviso2");
 //Criando o evento
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    if (inputNome.value.length <= 8) {
+    if (inputNome.value.length < 8) {
         aviso1.innerHTML = "Nome muito curto";
-        formNome.style.borderBottom = "2px solid red";
+        inputNome.style.borderBottom = "2px solid red";
+    }
+    if (inputSenha.value.length < 8) {
+        aviso2.innerHTML = "Senha muito curta";
     }
 });
